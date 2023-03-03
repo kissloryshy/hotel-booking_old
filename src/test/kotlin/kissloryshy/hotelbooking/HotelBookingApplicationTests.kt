7c64397555e7c3d010eebacab5297e8397df982e
kissloryshy <kissloryshy@gmail.com>
2023-03-03 20:08:10 +0300

    Init commit

---
+++ src/test/kotlin/kissloryshy/hotelbooking/HotelBookingApplicationTests.kt
@@ -0,0 +1,13 @@
+package kissloryshy.hotelbooking
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HotelBookingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
