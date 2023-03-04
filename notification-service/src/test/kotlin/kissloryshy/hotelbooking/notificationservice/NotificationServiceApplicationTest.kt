b9ea757bc241d61d93fa25f1684f0e1bcf373410
kissloryshy <kissloryshy@gmail.com>
2023-03-04 17:35:30 +0300

    discovery, gateway, notification services added

---
+++ notification-service/src/test/kotlin/kissloryshy/hotelbooking/notificationservice/NotificationServiceApplicationTest.kt
@@ -0,0 +1,14 @@
+package kissloryshy.hotelbooking.notificationservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotificationServiceApplicationTest {
+
+    @Test
+    fun contextLoads() {
+
+    }
+
+}
